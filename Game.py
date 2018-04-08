aced769237494da1bbef0087d96d83caa839de57
Emmanuel Iloh <emmanueli.work@gmail.com>
2018-04-08 19:12:29 +0800

    game loop
    the user can now choose to play multiple game or quit, and function have been broken down with main been less crowed

---
+++ Game.py
@@ -0,0 +1,92 @@
+import random
+import os
+
+def drawMan(numLives):
+    hangman =("""\n\t\t\t 122222222222222\n
+             1  2         2\n
+             1 2          33\n
+             12          3  3\n
+             1            33\n
+             1            3\n
+             1         5  4  5\n
+             1          5 4 5\n
+             1           545\n
+             1            4\n
+             1            4\n
+             1            4\n
+             1           6 6\n
+             1          6   6\n
+             1         6     6\n
+             1\n
+             1\n
+             0000000000000000000\n\n""")
+
+    for char in hangman:
+        try:
+            if int(char) < (7-numLives) :
+                print (char, end=' ')
+            else :
+                print (" ", end=' ')
+        except Exception as e:
+            print(char, end=' ')
+
+def readDict():
+    f = open("words.txt")
+    data = f.read()
+    dict = data.split("\n")
+    return dict
+
+def begin():
+    os.system('cls||clear')
+    dict = readDict()
+    random.seed()
+    word = dict[random.randrange(0, len(dict))]
+
+    numLives = 6
+    correct = False
+    missed = []
+
+    pool = [chr(letter) for letter in range(97, 123)]
+    right = []
+
+    while numLives > 0 :
+        check = 0
+        drawMan(numLives)
+        print ("lives = ", numLives-1)
+        print ("wrong guesses so far: ", ', '.join(missed))
+
+        for x in word :
+            if x in right :
+                print (x, end=' ')
+                check += 1
+            else :
+                print ("_ ", end=' ')
+
+        if  check == len(word):
+            correct = True
+            break
+
+        guess = input("\n\nenter a letter: ")
+        os.system('cls||clear')
+
+        if not guess in pool :
+            print ("\nyou have already guessed " + guess)
+        elif guess in word :
+            right.append(guess)
+            pool.remove(guess)
+            print ("\nyou're one step closer")
+        else :
+            print("\nyou guessed wrong")
+            missed.append(guess)
+            pool.remove(guess)
+            numLives -= 1
+
+    os.system('cls||clear')
+    drawMan(numLives)
+
+    if not correct :
+        print("\n\nBetter Luck next time")
+    else :
+        print("\n\n\n\nYou finally got it!!")
+
+    print("the Word was: " +word)
--- Main.py
+++ Main.py
@@ -1,89 +1,15 @@
-import random
 import os
+from Game import begin
 
 os.system('cls||clear')
 
-def drawMan(numLives):
-    hangman =("""\n\t\t\t 122222222222222\n
-             1  2         2\n
-             1 2          33\n
-             12          3  3\n
-             1            33\n
-             1            3\n
-             1         5  4  5\n
-             1          5 4 5\n
-             1           545\n
-             1            4\n
-             1            4\n
-             1            4\n
-             1           6 6\n
-             1          6   6\n
-             1         6     6\n
-             1\n
-             1\n
-             0000000000000000000\n\n""")
+while True :
+    option = input("""\n\nWhat do you want to do?
+    1. Play
+    2. quit\n >>>> """)
 
-    for char in hangman:
-        try:
-            if int(char) < (7-numLives) :
-                print (char, end=' ')
-            else :
-                print (" ", end=' ')
-        except Exception as e:
-            print(char, end=' ')
-
-
-f = open("words.txt")
-data = f.read()
-dict = data.split("\n")
-random.seed()
-word = dict[random.randrange(0, len(dict))]
-
-numLives = 6
-correct = False
-missed = []
-
-pool = [chr(letter) for letter in range(97, 123)]
-right = []
-
-while numLives > 0 :
-    check = 0
-    drawMan(numLives)
-    print ("lives = ", numLives-1)
-    print ("wrong guesses so far: ", ', '.join(missed))
-
-    for x in word :
-        if x in right :
-            print (x, end=' ')
-            check += 1
-        else :
-            print ("_ ", end=' ')
-
-    if  check == len(word):
-        correct = True
+    if option == "1":
+        begin()
+    else:
+        print("bye")
         break
-
-    guess = input("\n\nenter a letter: ")
-    os.system('cls||clear')
-
-    if not guess in pool :
-        print ("\nyou have already guessed " + guess)
-    elif guess in word :
-        right.append(guess)
-        pool.remove(guess)
-        print ("\nyou're one step closer")
-    else :
-        print("\nyou guessed wrong")
-        missed.append(guess)
-        pool.remove(guess)
-        numLives -= 1
-
-os.system('cls||clear')
-drawMan(numLives)
-
-if not correct :
-    print("\n\nBetter Luck next time")
-else :
-    print("\n\n\n\nYou finally got it!!")
-
-print("the Word was: " +word)
